3673f5d570e14bcc76356a1d2ef39d0df9fb464d
saiprasadkrishnan7 <saiprasadiyer2007@gmail.com>
2025-09-01 19:39:20 +0530

    database

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,36 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
+export const signup = async (req, res) => {
+    const {fullName, email, pasword} = req.body
+    try {
+        if (pasword.length < 6) {
+            return res.status(400).json({ message: "Passoword must be atleast 6 charecters" });
+        }
+
+        const user = await User.findOne({email})
+
+        if (user) return res.status(400).json({ message: "Email already exists" });
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName, 
+            email,
+            password: hashedPassword
+        })
+
+        if(newUser) {
+
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+
+        
+    } catch (error) {
+
+    }
 };
 
 export const login = (req, res) => {
